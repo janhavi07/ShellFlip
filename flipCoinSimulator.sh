fd14848e5f9400cfe887c3a41c1b95370cb4c24e
sunnym97 <15201030sunny@viva-technology.org>
2019-11-12 15:06:43 +0530

    % of heads and tails added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,13 +1,38 @@
 #!/bin/bash -x
+
+#VARIABLES
+headCount=0
+tailCount=0
+
+declare -A coin
+read -p "Enter number of times to flip the coin : " noOfTimes
+echo $noOfTimes
+ 
 function flip()
 {
 	echo $1
 }
+for(( time=1; time<=noOfTimes ; time++ ))
+do
 flipresult="$( flip $((RANDOM%2)) )"
 if [ $flipresult -eq 1 ]
 then
 	echo HEADS
+	headCount=$(( $headCount + 1))
+
 else
 	echo TAILS
-fi
+	tailCount=$(( $tailCount +1 ))
 
+fi
+done
+echo No of heads percentage
+headpercent=$(( ($headCount*100)/$noOfTimes ))
+coin[head]=$headpercent
+echo $headpercent
+
+echo No of Tails
+echo $tailCount
+tailpercent=$(( ($tailCount*100)/$noOfTimes ))
+coin[tail]=$tailpercent
+echo $tailpercent
