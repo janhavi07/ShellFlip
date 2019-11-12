e9842fbc6c894d1a4c9533328364fb161b85090c
sunnym97 <15201030sunny@viva-technology.org>
2019-11-12 18:06:30 +0530

    Doublet combination added

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,7 +4,7 @@
 headCount=0
 tailCount=0
 
-declare -A coin
+declare -A singlet
 read -p "Enter number of times to flip the coin : " noOfTimes
 echo $noOfTimes
  
@@ -26,13 +26,66 @@ else
 
 fi
 done
-echo No of heads percentage
+echo No of heads
+echo $headCount
 headpercent=$(( ($headCount*100)/$noOfTimes ))
-coin[head]=$headpercent
+singlet[head]=$headpercent
 echo $headpercent
 
 echo No of Tails
 echo $tailCount
 tailpercent=$(( ($tailCount*100)/$noOfTimes ))
-coin[tail]=$tailpercent
+singlet[tail]=$tailpercent
 echo $tailpercent
+
+declare -A doublet
+HHcout=0
+TTcount=0
+HTcount=0
+THcount=0
+function fliptwo()
+{
+	echo $1
+}
+for (( times=1; times<=noOfTimes; times++ ))
+do
+	coin1=$( fliptwo $((RANDOM%2)) )
+	coin2=$( fliptwo $((RANDOM%2)) )
+	if [ $coin1 -eq 1 ] && [ $coin2 -eq 1 ]
+	then
+		HHcount=$(( $HHcount +1 ))
+		echo $HHcount
+	elif [ $coin1 -eq 0 ] && [ $coin2 -eq 0 ]
+	then
+		TTcount=$(( $TTcount +1 ))
+	elif [ $coin1 -eq 0 ] && [ $coin2 -eq 1 ]
+	then
+		THcount=$(( $THcount +1 ))
+	elif [ $coin1 -eq 1 ] && [ $coin2 -eq 0 ]
+	then
+		HTcount=$(( $HTcount +1 ))
+	fi
+done
+
+echo percentage of HH
+HHP=$(( ($HHcount*100)/$noOfTimes ))
+doublet[HH]=$HHP
+echo ${doublet[HH]}
+
+echo perecentage of TT
+TTP=$(( ($TTcount*100)/$noOfTimes ))
+doublet[TT]=$TTP
+echo ${doublet[TT]}
+
+echo percentage of HT
+HTP=$(( ($HTcount*100)/$noOfTimes ))
+doublet[HT]=$HTP
+echo ${doublet[HT]}
+
+echo percentage of TH
+THP=$(( ($HHcount*100)/$noOfTimes ))
+doublet[TH]=$THP
+echo ${doublet[TH]}
+
+
+
